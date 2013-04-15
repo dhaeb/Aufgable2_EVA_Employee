44f9d69f058ac3bf39e36c2495ee3d37dee1f417
Dan Haeberlein <dan.haeberlein@googlemail.com>
2013-04-15 11:22:43 +0200

    changed mistake sal(e)ary in PersonUtil

--- src/main/de/eva/aufg2/PersonUtil.java
+++ src/main/de/eva/aufg2/PersonUtil.java
@@ -2,13 +2,13 @@ package de.eva.aufg2;
 
 public class PersonUtil {
 
-	public static Employee createEmployee(String firstName, String lastName, int id, int workload, Integer salery){
+	public static Employee createEmployee(String firstName, String lastName, int id, int workload, Integer salary){
 		Employee newEmployee = new Employee();
 		newEmployee.setFirstName(firstName);
 		newEmployee.setLastName(lastName);
 		newEmployee.setId(id);
 		newEmployee.setWorkingHoursPerWeek(workload);
-		newEmployee.setSalery(salery);
+		newEmployee.setSalary(salary);
 		return newEmployee;
 	}
 	
